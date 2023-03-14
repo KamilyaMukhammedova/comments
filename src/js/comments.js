e260bacfd209c94b4bf25f92f60894daece4afae
Kamilya Mukahmmedova <kamilichkam@gmail.com>
2023-03-14 16:37:39 +0500

    Реализация добавления или удаление лайка комментария

--- src/js/comments.js
+++ src/js/comments.js
@@ -1,17 +1,48 @@
 const commentsBlock = document.querySelector('#comments-block');
+const msg = document.querySelector('#msg');
 let commentsArray = [];
 
-function removeComment(commentId) {
-  const filteredComments = commentsArray.filter(comment => comment.id !== commentId);
-  localStorage.setItem('comments', JSON.stringify(filteredComments));
-  location.reload();
+function modifyComments(arr, commentId, value) {
+  const modifiedComments = arr.map(item => {
+    if (item.id === commentId) {
+      return {
+        ...item,
+        isFavorite: value
+      };
+    }
+
+    return item;
+  });
+  localStorage.setItem('comments', JSON.stringify(modifiedComments));
+}
+
+function getCommentsFromLocalStorage() {
+  return JSON.parse(localStorage.getItem('comments'));
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  commentsArray = JSON.parse(localStorage.getItem('comments'));
+  commentsArray = getCommentsFromLocalStorage();
+  const today = new Date().toISOString().slice(0, 10);
+  const yesterday = new Date().setDate(new Date().getDate() - 1);
+
+  if (!commentsArray || commentsArray.length === 0) {
+    msg.style.display = 'flex';
+  } else {
+    msg.style.display = 'none';
+  }
 
   if (commentsArray) {
-    commentsArray.reverse().forEach(comment => {
+    commentsArray.forEach(comment => {
+      let commentDate;
+
+      if (comment.date === today) {
+        commentDate = 'today,';
+      } else if (comment.date === new Date(yesterday).toISOString().slice(0, 10)) {
+        commentDate = 'yesterday,';
+      } else {
+        commentDate = comment.date;
+      }
+
       const commentCard = document.createElement('div');
       commentCard.className = 'comments__card';
       commentCard.innerHTML = `
@@ -26,10 +57,10 @@ document.addEventListener('DOMContentLoaded', () => {
                        id="${comment.id + 'favorite'}"
                   >
                       <path
-                            fill-rule="evenodd"
                             clip-rule="evenodd"
                             d="M12.0122 5.57169L10.9252 4.48469C8.77734 2.33681 5.29493 2.33681 3.14705 4.48469C0.999162 6.63258 0.999162 10.115 3.14705 12.2629L11.9859 21.1017L11.9877 21.0999L12.014 21.1262L20.8528 12.2874C23.0007 10.1395 23.0007 6.65711 20.8528 4.50923C18.705 2.36134 15.2226 2.36134 13.0747 4.50923L12.0122 5.57169ZM11.9877 18.2715L16.9239 13.3352L18.3747 11.9342L18.3762 11.9356L19.4386 10.8732C20.8055 9.50635 20.8055 7.29028 19.4386 5.92344C18.0718 4.55661 15.8557 4.55661 14.4889 5.92344L12.0133 8.39904L12.006 8.3918L12.005 8.39287L9.51101 5.89891C8.14417 4.53207 5.92809 4.53207 4.56126 5.89891C3.19442 7.26574 3.19442 9.48182 4.56126 10.8487L7.10068 13.3881L7.10248 13.3863L11.9877 18.2715Z"
                             fill="#885df1"
+                            id="${comment.id + 'path'}"
                       />
                   </svg>
                   <svg
@@ -58,17 +89,43 @@ document.addEventListener('DOMContentLoaded', () => {
                     <span class="comments__user-name">${comment.userName}</span>
                  </div>
                  <div class="comments__box2">
-                    <span class="comments__date">${comment.date} ${comment.time}</span>
+                    <span class="comments__date">
+                       ${commentDate} ${comment.time}
+                    </span>
                  </div>
              </div>
      `;
 
       commentsBlock.append(commentCard);
 
+      const path = document.getElementById(comment.id + 'path');
+
+      if (comment.isFavorite) {
+        path.setAttribute('fill-rule', 'nonzero');
+      } else {
+        path.setAttribute('fill-rule', 'evenodd');
+      }
+
       document.getElementById(comment.id + 'trash').addEventListener('click', () => {
-        removeComment(comment.id);
+        const filteredComments = commentsArray.filter(item => item.id !== comment.id);
+        localStorage.setItem('comments', JSON.stringify(filteredComments));
+        location.reload();
+      });
+
+      document.getElementById(comment.id + 'favorite').addEventListener('click', () => {
+        if (path.getAttribute('fill-rule') === 'evenodd') {
+          commentsArray = getCommentsFromLocalStorage();
+          modifyComments(commentsArray, comment.id, true);
+          path.setAttribute('fill-rule', 'nonzero');
+        } else {
+          commentsArray = getCommentsFromLocalStorage();
+          modifyComments(commentsArray, comment.id, false);
+          path.setAttribute('fill-rule', 'evenodd');
+        }
       });
     });
   }
 });
+
+
 
